b6b903cabe97ff0e47370784cdc5a878c431f5cd
sakayase <simon.ponitzki@gmail.com>
2020-11-23 17:32:22 +0100

    Ajout d'un fichier 'exemple' pour montrer l'utilisation des balises php

---
+++ tag.php
@@ -0,0 +1,24 @@
+<?php
+
+//code php
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tag</title>
+</head>
+<body>
+    <h2>
+        <?php echo 'lorem ipsum' ?>
+    </h2>
+    <p>
+        <?= 'lorem ipsum' ?> <!-- possible d'utiliser la balise < ? = sans echo car text -->
+    </p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos fugiat unde repudiandae vero natus ipsam et! Est, iure? Sequi eveniet quidem iste architecto sed fugit porro praesentium, quisquam maiores molestias.</p>
+</body>
+</html>
+<?php
+
+// PAS DE BALISE FERMANTE SI LE PHP TERMINE LE FICHIER 
