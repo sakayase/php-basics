4b339ac5fe04a4f13104a73bbd79a8819835eb36
sakayase <simon.ponitzki@gmail.com>
2020-11-23 13:53:25 +0100

    Ajout du script index.php pour la homepage
    Ce script affiche une page HTML avec le texte de la homepage

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <h1><?php echo 'Homepage' ?></h1>
+</body>
+</html>
