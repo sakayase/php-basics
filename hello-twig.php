814878cefe9970609d8c0dbf59314bd497bc509a
sakayase <simon.ponitzki@gmail.com>
2020-12-02 17:39:25 +0100

    Test de TWIG

---
+++ hello-twig.php
@@ -0,0 +1,24 @@
+<?php
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+// activation du système d'autoloading de Composer
+require __DIR__.'/vendor/autoload.php';
+
+// instanciation du chargeur de templates
+$loader = new FilesystemLoader(__DIR__.'/templates');
+
+// instanciation du moteur de template
+$twig = new Environment($loader);
+
+// initialisation d'une donnée
+$greeting = 'Hello Twig!';
+$paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, provident. Ea molestias dolor ad totam officiis. Laudantium nobis praesentium molestiae?';
+
+// affichage du rendu d'un template
+echo $twig->render('hello-twig.html.twig', [
+    // transmission de données au template
+    'greeting' => $greeting,
+    'paragraph' => $paragraph,
+]);
